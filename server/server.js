afa4dedeb5f720d82d94a200ad60c8a90ddd3259
Alexander Nilsson <a@alexandernilsson.com>
2013-06-08 17:23:16 +0200

    added example responses to the server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
 var io = require('socket.io').listen(80);
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
+  // TODO: send 'client:update' to all sockets for this new connection
+
+  socket.on('login', function (data) {
+    socket.username = data.username;
+    socket.pubkey = data.pubkey;
+    socket.emit('login:reply', {
+      status: "ok"
+      // TODO: Iterate over sockets, compile a list of clients
+    });
+  });
+  socket.on('send:message', function (data) {
+    // TODO: Iterate over sockets, find socket with username data.username, and send it to them, then send reply on success
+    socket.emit('send:message', {
+      status: "ok"
+    });
     console.log(data);
   });
+
 });
