cfa346a222a4c7ca81a36d787c3f44fec57d70a1
Alexander Nilsson <a@alexandernilsson.com>
2013-06-08 18:06:08 +0200

    i think the server is done. knock on wood.

--- server/server.js
+++ server/server.js
@@ -1,18 +1,51 @@
 var io = require('socket.io').listen(80);
 
 io.sockets.on('connection', function (socket) {
-  // TODO: send 'client:update' to all sockets for this new connection
-
   socket.on('login', function (data) {
-    socket.username = data.username;
-    socket.pubkey = data.pubkey;
+    socket.set('username', data.username);
+    socket.set('pubkey', data.pubkey);
+
+    // Build client list
+    var client_list = {};
+    for (var key in io.sockets) {
+      if (io.sockets[key].get('username') != data.username) {
+        client_list[data.username] = {
+          pubkey: io.sockets[key].get('pubkey')
+        };
+      }
+    }
+
+    // Send login success, because it will always succeed.
     socket.emit('login:reply', {
-      status: "ok"
-      // TODO: Iterate over sockets, compile a list of clients
+      status: 'ok',
+      clientlist: client_list
     });
+
+    // Build client update object
+    var client_update = {};
+    client_update[data.username] = {
+      pubkey: data.pubkey,
+      status: "online"
+    };
+
+    // Broadcast the new client
+    socket.broadcast.emit('client:update', client_update);
   });
   socket.on('send:message', function (data) {
-    // TODO: Iterate over sockets, find socket with username data.username, and send it to them, then send reply on success
+    // Build message
+    var msg = {};
+    msg.id = data.id;
+    msg.sender = socket.get('username');
+    msg.data = data.data;
+
+    // Send message 
+    for (var key in io.sockets) {
+      if (io.sockets[key].get('username') == data.recipient) {
+        io.sockets[key].emit('recieve:message', msg);
+      }
+    }
+
+    // Send message status
     socket.emit('send:message', {
       status: "ok"
     });
