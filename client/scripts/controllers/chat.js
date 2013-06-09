34e538db7c74b573fd5bc768eb4eb70e1dfe3ea4
Alexander Nilsson <a@alexandernilsson.com>
2013-06-09 11:11:33 +0200

    scrolling stuff

--- client/scripts/controllers/chat.js
+++ client/scripts/controllers/chat.js
@@ -42,7 +42,11 @@ app.controller('ChatCtrl', function ($scope, socket, localStorageService) {
     };
 
     $scope.$on('event:message_received', function () {
+        //var move = ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) ? true : false;
         $scope.messages_read[$scope.params.recipient] = $scope.messages[$scope.params.recipient].length;
+        if ($('.messages li:last-child').isOnScreen()) {
+            $("html, body").animate({ scrollTop: $(document).height() }, 300);
+        }
     });
 
 });
