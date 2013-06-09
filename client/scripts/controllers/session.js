3b39a535eaf1362ef5b0a3f6ee5f572d6dd358d4
Traxmaxx <github@netzok.net>
2013-06-09 12:04:08 +0200

    Align input and add loading dialog on login

--- client/scripts/controllers/session.js
+++ client/scripts/controllers/session.js
@@ -2,6 +2,7 @@
 
 app.controller('SessionCtrl', function ($scope, socket, localStorageService, $location, $rootScope) {
     $scope.login = function () {
+        $scope.loading = 'loading';
         localStorageService.save('rsa', cryptico.generateRSAKey($scope.passphrase, $scope.bitLength).toJSON());
         localStorageService.save('username', $scope.username);
         $rootScope.$broadcast('event:auth-successful');
