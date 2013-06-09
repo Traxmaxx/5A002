081411d8e2af71a60d673d35bcead4d5e961264d
ldx <v.nebehaj@gmail.com>
2013-06-09 05:18:56 +0200

    Revert "I don't know what I'm doing haha"
    This reverts commit e21335fe4de9c7f408cabcc11b6d319dce4fe481.

--- server/server.js
+++ server/server.js
@@ -136,7 +136,6 @@ io.sockets.on('connection', function (socket) {
   socket.on('logout', function(derp) {
     if (clients[socket.id] !== undefined) {
       log.info("'" + clients[socket.id].username + "' logged out.");
-      clients[socket.id].socket.close();
       delete clients[socket.id];
 
       client_list = {};
